8665886fea9ccc52ea8161fb8f086fb5539a30e6
Basia <barbara.kasieczka@gmail.com>
2016-12-07 02:12:53 +0100

    Szukanie POI w zadanym obszarze

--- pipsg_cw3_git/poimanager.py
+++ pipsg_cw3_git/poimanager.py
@@ -1,10 +1,12 @@
 from poi import POI
 import csv
+import math
 pois=[]
 
 class POIManager(object):
   def __init__(self):
     self.pois = []
+    self.odp=[]
   
   def get_all_poi(self):
     with open('pois.txt', 'r') as plik:
@@ -16,7 +18,14 @@ class POIManager(object):
   
   def add_poi(self,POI):
     self.pois.append(POI)
-    return self.pois
+    pass
     
   def get_poi_near(self,x,y,r):
-    pass
+    self.x=x
+    self.y=y
+    self.r=r
+    for a in self.pois:
+      if math.sqrt((float(a.x)-float(self.x))**2+(float(a.y)-float(self.y))**2)<float(r):
+        self.odp.append(a)
+    return self.odp
+
